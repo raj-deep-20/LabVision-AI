81944fef2c13aa75f81d42d2b7bc809c567b412e
Rajdeep Podder <160866177+raj-deep-20@users.noreply.github.com>
2026-08-02 17:43:59 +0530

    Resolved CORS to allow frontend domain

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,6 +20,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "https://lab-vision-ai.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
